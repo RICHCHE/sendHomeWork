de3a4081b6bce4a4e02ca87ffca359363db56169
RICHCHE <Poorman007za@gmail.com>
2022-05-21 10:02:58 +0700

    sendHomeWork

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AppicationCalulation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_1_Click(object sender, RoutedEventArgs e)
+        {
+            int day = int.Parse(txtBox_3.Text) / (int.Parse(txtBox_1.Text) - int.Parse(txtBox_2.Text));
+            (txtBox_4.Text) = day.ToString();
+        }
+    }
+}
